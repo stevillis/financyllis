c6df25bced86fd02ec6d056068083d591c377427
Stévillis Sousa <stevillis@hotmail.com>
2023-09-30 17:55:50 -0400

    feat: create menu bar

--- app.py
+++ app.py
@@ -6,3 +6,42 @@ import pandas as pd
 import seaborn as sns
 import streamlit as st
 import yfinance as yf
+
+MENU_ITEMS = ["Home", "Panorama do Mercado", "Mapa Mensal", "Fundamentos"]
+
+
+def home():
+    pass
+
+
+def overview():
+    pass
+
+
+def monthly_map():
+    pass
+
+
+def fundamentals():
+    pass
+
+
+def main():
+    st.sidebar.image("logo_financyllis_transparent_bg.png", width=100)
+    st.sidebar.title("Financyllis")
+    st.sidebar.markdown("---")
+
+    menu_choice = st.sidebar.radio("Escolha a opção", MENU_ITEMS)
+
+    if menu_choice == MENU_ITEMS[0]:  # Home
+        home()
+    if menu_choice == MENU_ITEMS[1]:  # Panorama do Mercado
+        overview()
+    if menu_choice == MENU_ITEMS[2]:  # Mapa Mensal
+        monthly_map()
+    if menu_choice == MENU_ITEMS[3]:  # Fundamentos
+        fundamentals()
+
+
+if __name__ == "__main__":
+    main()
